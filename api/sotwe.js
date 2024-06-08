9d1d3d5918806838c2b7edef9e12a28e1f70ad5d
Golfot <rizkifundie@gmail.com>
2024-06-08 11:05:38 +0700

    Update sotwe.js

--- api/sotwe.js
+++ api/sotwe.js
@@ -1,38 +1,18 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    // Menambahkan header CORS ke dalam respons
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    // Mengatasi preflight request (OPTIONS)
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
+    const { username } = req.query;
+    const apiUrl = `https://api.sotwe.com/v3/user/${username}`;
 
     try {
-        const response = await fetch('https://api.sotwe.com/v3/user/aditendeur');
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`Error fetching data from ${apiUrl}: ${response.statusText}`);
+        }
         const data = await response.json();
 
-        const result = {
-            after: data.after,
-            videos: data.data.map(post => {
-                const videoEntities = post.mediaEntities || post.retweetedStatus?.mediaEntities;
-                if (videoEntities && videoEntities.length > 0) {
-                    const videoUrl = videoEntities[0].videoInfo?.variants?.find(variant => variant.type === 'video/mp4')?.url || '';
-                    const displayURL = videoEntities[0].displayURL || '';
-                    const type = videoEntities[0].type || '';
-                    return { displayURL, type };
-                }
-                return null;
-            }).filter(video => video !== null)
-        };
-
-        res.status(200).json(result);
+        res.status(200).json(data);
     } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).json({ error: 'Error' });
+        res.status(500).json({ error: error.message });
     }
 };
