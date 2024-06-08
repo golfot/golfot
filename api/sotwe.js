2d47c7976b07cdf230afaf7e7371e564af355443
Golfot <rizkifundie@gmail.com>
2024-06-08 11:17:07 +0700

    Update sotwe.js

--- api/sotwe.js
+++ api/sotwe.js
@@ -1,22 +1,16 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const username = req.query.username;
-    if (!username) {
-        res.status(400).json({ error: 'Username is required' });
-        return;
-    }
+  const url = 'https://api.sotwe.com/v3/user/aditendeur';
 
-    const apiUrl = `https://api.sotwe.com/v3/user/${username}`;
-
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`Error fetching data from ${apiUrl}: ${response.statusText}`);
-        }
-        const data = await response.json();
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.statusText}`);
     }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
