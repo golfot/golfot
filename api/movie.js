1f47afcfc0d00b17191113c6d22f9791118a4cc0
Golfot <rizkifundie@gmail.com>
2024-06-14 13:39:39 +0700

    Update movie.js

--- api/movie.js
+++ api/movie.js
@@ -1,16 +1,38 @@
-const request = require('request');
+const puppeteer = require('puppeteer');
 
-export default function handler(req, res) {
-    const videoUrl = req.query.url;
+(async () => {
+    // Mulai browser Puppeteer
+    const browser = await puppeteer.launch();
 
-    // Set the referer header to your domain
-    const options = {
-        url: videoUrl,
-        headers: {
-            'Referer': 'https://artist.dutamovie21.cloud/hit-man-2023/'
+    // Buka halaman baru
+    const page = await browser.newPage();
+
+    // Array untuk menyimpan data URL yang ditangkap
+    let capturedUrls = [];
+
+    // Tangkap semua permintaan jaringan
+    page.on('request', request => {
+        const url = request.url();
+        console.log('Request URL:', url);
+
+        // Jika URL mengandung "master.m3u8", tambahkan ke array capturedUrls
+        if (url.includes('master.m3u8')) {
+            capturedUrls.push(url);
+            console.log('Captured URL:', url);
         }
-    };
+    });
 
-    // Pipe the request to the response
-    request(options).pipe(res);
-}
+    // Buka halaman web target
+    const targetUrl = 'https://artist.dutamovie21.cloud/sweet-release-2024/?player=3'; // Ganti dengan URL target
+    await page.goto(targetUrl, { waitUntil: 'networkidle2' });
+
+    // Tunggu beberapa detik untuk memastikan semua permintaan telah ditangkap
+    await page.waitForTimeout(10000); // 10 detik
+
+    // Tutup browser
+    await browser.close();
+
+    // Mengkonversi nilai menjadi format JSON
+    const jsonData = JSON.stringify({ capturedUrls }, null, 2);
+    console.log(jsonData);
+})();
