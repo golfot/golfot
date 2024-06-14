51c137d2a692f6ea5b32ec1f19a188908cc1b461
Golfot <rizkifundie@gmail.com>
2024-06-14 13:04:00 +0700

    Update movie.js

--- api/movie.js
+++ api/movie.js
@@ -1,43 +1,16 @@
-const axios = require('axios');
-const express = require('express');
-const app = express();
-const port = 3000;
+const request = require('request');
 
-async function loadUrlWithReferer() {
-    try {
-        const response = await axios.get('https://vidhidepre.com/embed/v5xyj2j9puch', {
-            headers: {
-                'Referer': 'https://artist.dutamovie21.cloud/'
-            }
-        });
-        return response.data;
-    } catch (error) {
-        console.error('Error loading the URL:', error);
-        return null;
-    }
+export default function handler(req, res) {
+    const videoUrl = req.query.url;
+
+    // Set the referer header to your domain
+    const options = {
+        url: videoUrl,
+        headers: {
+            'Referer': 'https: //artist.dutamovie21.cloud/'
+        }
+    };
+
+    // Pipe the request to the response
+    request(options).pipe(res);
 }
-
-app.get('/api/movie', async (req, res) => {
-    const data = await loadUrlWithReferer();
-    if (data) {
-        res.send(`
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Loaded URL</title>
-            </head>
-            <body>
-                <iframe srcdoc="${data}" width="100%" height="600" frameborder="0"></iframe>
-            </body>
-            </html>
-        `);
-    } else {
-        res.send('Failed to load the URL');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-});
